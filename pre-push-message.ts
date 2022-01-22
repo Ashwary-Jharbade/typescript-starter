ff7eee2a2902638b999816a03fbc45ca3544f0f6
Ashwary <ashwary.jharbade999@gmail.com>
2022-01-23 01:36:15 +0530

    first commit

--- pre-push-message.ts
+++ pre-push-message.ts
@@ -1,22 +1,5 @@
-import readline from 'readline';
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-const prompt = `
-    ######################################################
-    #       Comrade, we are publishing you changes       #
-    ######################################################
-`;
-
-rl.question(prompt, function () {
-  rl.question('Do you want to countnue ? ', function (answer) {
-    console.log(`${answer}`);
-    rl.close();
-  });
-});
-
-rl.on('close', function () {
-  process.exit(0);
-});
+console.log(`
+######################################################
+#       Comrade, we are publishing you changes       #
+######################################################
+`);
