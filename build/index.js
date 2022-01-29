1f5a353d1bcb12fcde8020057afaf1b9e5d66801
Ashwary <ashwary.jharbade999@gmail.com>
2022-01-29 22:59:10 +0530

    Added few changes

--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = __importDefault(require("./test/index"));
-(0, index_1.default)(1, 1);
-const x = 1;
-x + x;
