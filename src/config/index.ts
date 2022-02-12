3560161eff065c3159fc54bcb5109e0aa1aeba25
Ashwary <ashwary.jharbade999@gmail.com>
2022-02-12 21:41:39 +0530

    Added config

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,9 +4,9 @@ class Config {
   constructor() {
     nconf.argv().env('_');
     const environment = nconf.get('NODE:ENV');
-    const envPath = path.join(__dirname, `${environment}.json`);
+    const envPath = path.join(__dirname, `../../config/${environment}.json`);
     nconf.file(environment, envPath);
-    const defaultPath = path.join(__dirname, 'default.json');
+    const defaultPath = path.join(__dirname, '../../config/default.json');
     nconf.file('default', defaultPath);
   }
   get(key: string) {
